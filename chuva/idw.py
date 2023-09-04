fbda4e1c7586843fd69d1a76c42fb058b9d0641a
bortolletto <felipebortolletto0@gmail.com>
2023-09-04 20:46:54 -0300

    Avanços na espacialização, no entanto os valores utilizados não condizem com a realidade o IDW teve a tendencia de diminuir os resultados obtidos. Sera testado um novo modelo.

--- chuva/idw.py
+++ chuva/idw.py
@@ -13,15 +13,16 @@ import numpy as np
 
 locais = pd.read_csv("./csvs/loc_pontos_georeferenciado.csv",index_col = 0)
 dados = pd.read_csv("./csvs/chuva_pontos.csv",index_col = 0,parse_dates = True)
+
 estac = pd.read_csv("./csvs/estac_atualizado.csv",index_col = 0)
 
 
-base = xr.open_dataset("/discolocal/felipe/lisflood_pm/catch/outlets.nc").to_dataframe()
-base_temp = xr.open_dataset("/discolocal/felipe/lisflood_pm/catch/meteo/pr.nc")
+base = xr.open_dataset("../../lf_pm/catch/maps/outlets.nc").to_dataframe()
+base_temp   = xr.open_dataset("../../lf_pm/catch/meteo/chuvas/pr.nc")
 
 
 base_temp = base_temp.drop("spatial_ref")
-
+# base_temp = base_temp.to_frame()
 base = base.reset_index()
 pontos = base[["x","y"]]
 
@@ -34,19 +35,21 @@ def calcular_distancia(x1, y1, x2, y2):
 
 
 #%%
-p = 2
-dados = dados["2013":]
+p = 0.5
+dados = dados["2013":"2023-04-07"]
+ano = 2013
 for ano in dados.index.year.unique():
     validada = estac.loc[estac.index == ano]
     selected_stations = validada.columns[validada.isin([True]).any()]
-    temp = dados[selected_stations][str(ano)]
-    temp_loc = locais.loc[locais.codigo.isin([int(x) for x in selected_stations]) == True]
+    temp = dados[selected_stations].loc[str(ano)]
+    temp_loc = locais.loc[locais.codigo.isin([int(x) for x in selected_stations]) == True].copy()
     
     for x,y in zip(pontos["x"],pontos["y"]):
 
         temp_loc['distancia'] = temp_loc.apply(lambda row: calcular_distancia(x, y, row['cordx'], row['cordy']), axis=1)
+        # temp_loc["distancia"] = temp_loc['distancia']/1000
         somatorio_distancias = sum([ x**-p for x in temp_loc["distancia"]])
-        temp_loc['h'] = [ (x**-p/somatorio_distancias) for x in temp_loc["distancia"]]
+        temp_loc['h'] = [(x**-p/somatorio_distancias) for x in temp_loc["distancia"]]
         
         for estacao in temp.columns:
 
@@ -54,6 +57,7 @@ for ano in dados.index.year.unique():
             
         chuva_ano = temp.sum(axis = 1).to_frame()
         chuva_ano.rename(columns = {0:"chuva"},inplace = True)
+        chuva_ano = chuva_ano.resample("D").sum(min_count = 20)
         # indice_x = (base_temp['x'] == x).argmax()
         # indice_y = (base_temp['y'] == y).argmax()
         
@@ -62,6 +66,6 @@ for ano in dados.index.year.unique():
             indice_t = (base_temp['time'] == tempo).argmax()
             novo_valor = chuva_ano.loc[chuva_ano.index == tempo,"chuva"].values[0]
             base_temp.loc[{'time': tempo, 'x': x, 'y':y}] = novo_valor
-
+    print(f"Estamos no ano de :{ano}")
             
-        #multiplicar h por indice de cada coluna
+base_temp.to_netcdf("../../lf_pm/catch/meteo/chuvas/nova_chuva_atualiziada.nc")
